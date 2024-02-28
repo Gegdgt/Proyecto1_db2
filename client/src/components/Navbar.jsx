c52f03a29c4c7a39a492eb534b93c6f866c09a0b
SebasJuarez <juaressebastian@gmail.com>
2024-02-27 23:57:27 -0600

    v2.5

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,6 +10,7 @@ function Navbar() {
         {/* Aquí puedes agregar lógica para la funcionalidad de búsqueda si es necesario */}
       </div>
       <div className="navbar-links">
+        <a href="/login">Iniciar sesión</a>
         <a href="/user">Usuarios</a>
         <a href="Create">Crear</a>
       </div>
